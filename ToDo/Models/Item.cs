943723ab3a329e623cfbaa1ca791d46c5dadd3d0
DStanarevic <DejanStanarevic@gmail.com>
2017-10-27 10:38:01 -0400

    Item added

--- ToDo/Models/Item.cs
+++ ToDo/Models/Item.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -14,5 +15,9 @@ namespace ToDo.Models
         public DateTime DueDate { get; set; }
         public string Details { get; set; }
         public bool IsDone { get; set; } //Bools always have to be named in the positive.
+
+        [ForeignKey("List")]
+        public int ListID { get; set; }  // This sets the foreign key from the other table.
+        public virtual List List { get; set; } // This creates the connection to the other class.
     }
 }
