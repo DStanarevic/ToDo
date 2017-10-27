3a854ac3ab4021bbb94eed70edabfb8151b54c41
DStanarevic <DejanStanarevic@gmail.com>
2017-10-27 10:24:53 -0400

    Item model added

---
+++ ToDo/Models/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ToDo.Models
+{
+    public class Item
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime DueDate { get; set; }
+        public string Details { get; set; }
+        public bool IsDone { get; set; } //Bools always have to be named in the positive.
+    }
+}
